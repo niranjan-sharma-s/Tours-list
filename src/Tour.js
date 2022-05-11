d62b15bd844b2c3d1be2549be01a435a16ff6ddd
niranjan-sharma-s <niranjansharma1992@gmail.com>
2022-05-11 16:08:49 +0530

    feat-added-tours-list

---
+++ src/Tour.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+
+const Tour = ({removeTour , ...tour}) =>{
+
+  const [readMore, setReadMore] = useState(false)
+  return <article className='single-tour'>
+    <img src={tour.image} alt={tour.name} />
+    <footer>
+      <div className="tour-info">
+        <h4>{tour.name}</h4>
+        <h4 className="tour-price" >$ {tour.price} </h4>
+      </div>
+      <p>{readMore ? tour.info :`${tour.info.substring(0,200)}...`  }
+      <button onClick ={() => setReadMore(!readMore)}>
+        {readMore  ? 'show less' : 'read more'}
+      </button></p>
+      <button className='delete-btn' onClick={()=> removeTour(tour.id)}> not interested</button>
+    </footer>
+  
+
+
+  </article>
+}
+export default Tour;
